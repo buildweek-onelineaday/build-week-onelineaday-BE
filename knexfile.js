f8c55fcf6c2e9d05632d162e619dccd79a6e9b6e
Cody Brewer <brewer.cody.m@gmail.com>
2019-07-11 21:42:13 -0600

    set up for postgres and knex

---
+++ knexfile.js
@@ -0,0 +1,39 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/5000',
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds/dev'
+    },
+    useNullAsDefault: true
+  },
+
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/5000',
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/test'
+    },
+    useNullAsDefault: true
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/production'
+    },
+    useNullAsDefault: true
+  }
+};
