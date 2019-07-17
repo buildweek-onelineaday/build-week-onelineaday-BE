4d32cea7fce49ff82ce0c7b0075dd1537c722a0c
Memitaru <amandatwah@gmail.com>
2019-07-16 17:05:02 -0700

    Can now get all posts in descending order and post new posts

--- api/routes/postsHelpers.js
+++ api/routes/postsHelpers.js
@@ -1,1 +1,21 @@
 const db = require('../../data/dbConfig');
+
+module.exports = {
+    getPosts,
+    addPost,
+    getPostById
+}
+
+function getPosts(){
+    return db('posts').orderBy('id', 'desc')
+}
+
+function getPostById(id){
+    return db('posts').where({id}).first()
+}
+
+async function addPost(post){
+    const [id] = await db('posts').insert(post)
+
+    return getPostById(id)
+}
--- api/routes/postsRouter.js
+++ api/routes/postsRouter.js
@@ -2,6 +2,38 @@ const router = require('express').Router();
 
 const Posts = require('./postsHelpers');
 
+// Returns all posts in descending order
 
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Posts.getPosts(req.body)
+        if (posts){
+            res.status(200).json(posts)
+        } else {
+            res.status(404).json({error: "No posts found"})
+        }
+    }
+    catch(err){
+        res.status(500).json(err)
+    }
+})
+
+router.post('/', requiredBody, async (req, res) => {
+    try {
+        const post = await Posts.addPost(req.body)
+        res.status(201).json(post)
+    }
+    catch(err){
+        res.status(500).json(err)
+    }
+})
+
+function requiredBody(req, res, next){
+    if (req.body && Object.keys(req.body).length){
+      next();
+    } else {
+      next({message: "Please include request body"})
+    }
+  }
 
 module.exports = router;
