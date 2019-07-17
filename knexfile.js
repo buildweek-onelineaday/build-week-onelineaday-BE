4d968192656639e066cdb1671280a91003517d87
Cody Brewer <brewer.cody.m@gmail.com>
2019-07-16 22:17:08 -0600

    users table created

--- knexfile.js
+++ knexfile.js
@@ -1,39 +1,29 @@
-// Update with your config settings.
-
 module.exports = {
   development: {
-    client: 'pg',
-    connection: 'postgres://localhost/5000',
+    client: 'sqlite3',
+    connection: {
+      filename: './data/games.sqlite3'
+    },
+
+    useNullAsDefault: true,
     migrations: {
       directory: './data/migrations'
     },
     seeds: {
-      directory: './data/seeds/dev'
-    },
-    useNullAsDefault: true
+      directory: './data/seeds'
+    }
   },
-
-  test: {
-    client: 'pg',
-    connection: 'postgres://localhost/5000',
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/test.sqlite3'
+    },
+    useNullAsDefault: true,
     migrations: {
       directory: './data/migrations'
     },
     seeds: {
-      directory: './db/seeds/test'
-    },
-    useNullAsDefault: true
-  },
-
-  production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL,
-    migrations: {
-      directory: './db/migrations'
-    },
-    seeds: {
-      directory: './db/seeds/production'
-    },
-    useNullAsDefault: true
+      directory: './data/seeds'
+    }
   }
 };
