32535940d5308be5b3c59762cb0fbfcd98a4ebe1
CodyBrewer <brewer.cody.m@gmail.com>
2019-07-19 15:51:13 -0600

    jwt fix

--- auth/index.js
+++ auth/index.js
@@ -47,7 +47,7 @@ const validateUser = async (req, res, next) => {
   }
 };
 
-function generateToken(user) {
+const generateToken = (user) => {
   const payload = {
     subject: user.id,
     username: user.username
@@ -58,23 +58,23 @@ function generateToken(user) {
   };
 
   return jwt.sign(payload, secrets.jwtKey, options);
+}
 
-  function authenticate(req, res, next) {
-    const token = req.get('Authorization');
-  
-    if (token) {
-      jwt.verify(token, jwtKey, (err, decoded) => {
-        if (err) return res.status(401).json(err);
-  
-        req.decoded = decoded;
-  
-        next();
-      });
-    } else {
-      return res.status(401).json({
-        error: 'No token provided, must be set on the Authorization Header',
-      });
-    }
+const authenticate = (req, res, next) => {
+  const token = req.get('Authorization');
+
+  if (token) {
+    jwt.verify(token, jwtKey, (err, decoded) => {
+      if (err) return res.status(401).json(err);
+
+      req.decoded = decoded;
+
+      next();
+    });
+  } else {
+    return res.status(401).json({
+      error: 'No token provided, must be set on the Authorization Header',
+    });
   }
 };
 module.exports = { hash, validate, validateUser, generateToken, authenticate };
