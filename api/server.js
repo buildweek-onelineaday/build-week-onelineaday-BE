11ca918b818c31366a74831bc0ef0d8973f5835a
Cody Brewer <brewer.cody.m@gmail.com>
2019-07-18 20:56:32 -0600

    changed endponts from api/*** to just /***

--- api/server.js
+++ api/server.js
@@ -4,16 +4,16 @@ const helmet = require('helmet');
 const registerRouter = require('../controllers/register');
 const loginRouter = require('../controllers/login');
 const usersRouter = require('../controllers/users');
-const postsRouter = require('../api/routes/postsRouter')
+const postsRouter = require('../api/routes/postsRouter');
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
-server.use('/api/register', registerRouter);
-server.use('/api/login', loginRouter);
-server.use('/api/users', usersRouter);
-server.use('/posts', postsRouter)
+server.use('/register', registerRouter);
+server.use('/login', loginRouter);
+server.use('/users', usersRouter);
+server.use('/posts', postsRouter);
 
 server.get('/', (req, res) => {
   res.status(200).json({ message: 'API is Up' });
