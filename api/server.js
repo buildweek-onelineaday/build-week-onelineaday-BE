a52a69973d858359650a1445ee48e4e2b1755a04
Memitaru <amandatwah@gmail.com>
2019-07-21 17:01:44 -0700

    Protected users and posts routes

--- api/server.js
+++ api/server.js
@@ -5,6 +5,7 @@ const registerRouter = require('../controllers/register');
 const loginRouter = require('../controllers/login');
 const usersRouter = require('../controllers/users');
 const postsRouter = require('../api/routes/postsRouter');
+const {authenticate} = require('../auth/index')
 
 const server = express();
 
@@ -13,8 +14,8 @@ server.use(cors());
 server.use(express.json());
 server.use('/register', registerRouter);
 server.use('/login', loginRouter);
-server.use('/users', usersRouter);
-server.use('/posts', postsRouter);
+server.use('/users', authenticate, usersRouter);
+server.use('/posts', authenticate, postsRouter);
 
 server.get('/', (req, res) => {
   res.status(200).json({ message: 'API is Up' });
